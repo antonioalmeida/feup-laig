188b377e12a25d3e7a98372960f7406d82dd34a5
cyrilico <jpdamas15@gmail.com>
2017-11-07 22:50:19 +0000

    Adjust linear animation to support any number of segments

--- tp1/MyLinearAnimation.js
+++ tp1/MyLinearAnimation.js
@@ -3,24 +3,27 @@ function MyLinearAnimation(id, velocity, controlPoints) {
     this.velocity = velocity;
     this.controlPoints = [];
 
-    //For each segment
-    this.rotationAngles = [];
-    this.velocities = [];
+    this.rotationAngles = []; //Orientation for each segment
+    this.times = []; //Time for each segment
+    this.numLines = 0; //Number of segments
+    this.currentSegment = 0; //Index of current segment
+    //this.velocities = [];
 
-    let length = 0;
-    //Calculate initial (constant) values: vx, vy and rotation angle (for each line segment)
+    let totalLength = 0;
     for(let i = 0; i < controlPoints.length; ++i){
         this.controlPoints.push(vec3.fromValues(controlPoints[i][0], controlPoints[i][1], controlPoints[i][2]));
         if(i > 0){
             let currAngle = -Math.atan2(this.controlPoints[i][2]- this.controlPoints[i-1][2], this.controlPoints[i][0] - this.controlPoints[i-1][0]); //Not sure if works
             this.rotationAngles.push(currAngle);
-            this.velocities.push([velocity*Math.cos(currAngle), velocity*Math.sin(currAngle)]);
-            length += vec3.dist(this.controlPoints[i], this.controlPoints[i-1]);
+            //this.velocities.push([velocity*Math.cos(currAngle), velocity*Math.sin(currAngle)]);
+            let currentLength = vec3.dist(this.controlPoints[i], this.controlPoints[i-1]);
+            this.times.push(currentLength / this.velocity);
+            totalLength += currentLength;
+            ++this.numLines;
         }
     }
 
-    this.animationTime = length / this.velocity;
-    console.log("time: "+this.animationTime);
+    this.animationTime = length / this.velocity; //Total animation time
 };
 
 MyLinearAnimation.prototype = Object.create(MyAnimation.prototype);
@@ -28,12 +31,15 @@ MyLinearAnimation.prototype.constructor = MyLinearAnimation;
 
 MyLinearAnimation.prototype.update = function(currTime) {
     MyAnimation.prototype.update.call(this, currTime);
-    this.delta %= this.animationTime; //To ensure animation repetition (for now, at least)
-
+    if(this.delta > this.times[this.currentSegment]) {
+        this.delta = 0;
+        this.currentSegment = (this.currentSegment + 1) % this.numLines; //To ensure animation loop (at least for now)
+        this.startTime = currTime;
+    }
+    
     mat4.identity(this.currentMatrix);
     let currentPoint = vec3.create();
-    vec3.lerp(currentPoint, this.controlPoints[0], this.controlPoints[1], this.delta / this.animationTime);
-    //console.log("New Point: "+currentPoint[0]+" "+currentPoint[1]+" "+currentPoint[2]);
+    vec3.lerp(currentPoint, this.controlPoints[this.currentSegment], this.controlPoints[this.currentSegment+1], this.delta / this.times[this.currentSegment]);
     mat4.translate(this.currentMatrix, this.currentMatrix, currentPoint);
-    mat4.rotateY(this.currentMatrix, this.currentMatrix, this.rotationAngles[0]);
+    mat4.rotateY(this.currentMatrix, this.currentMatrix, this.rotationAngles[this.currentSegment]);
 }
