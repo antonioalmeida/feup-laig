27e998dc1ea20e41b9740e8c167132ee69b2232d
cyrilico <jpdamas15@gmail.com>
2017-11-08 13:17:23 +0000

    Finish (although lacks testing) bezier animation

--- tp1/MyBezierAnimation.js
+++ tp1/MyBezierAnimation.js
@@ -8,7 +8,7 @@ function MyBezierAnimation(id, velocity, controlPoints) {
 
     //Calculate initial constant values
     this.curveLength = this.approximateCurveLength();
-    this.totalTime = this.curveLength/velocity;
+    this.animationTime = this.curveLength/velocity;
 }
 
 MyBezierAnimation.prototype = Object.create(MyAnimation.prototype);
@@ -34,4 +34,41 @@ MyBezierAnimation.prototype.approximateCurveLength = function() {
 
 MyBezierAnimation.prototype.update = function(currTime) {
     MyAnimation.prototype.update.call(this, currTime);
+
+    this.delta %= this.animationTime; //To ensure animation loop (for now, at least)
+    let s = this.delta/this.animationTime;
+    let currentQ = this.Q(s);
+    let currentDerivativeQ = this.derivativeQ(s);
+    let currentDerivativeNorm = vec3.length(currentDerivativeQ);
+    let cos = currentQ[0] / currentDerivativeNorm;
+    let sin = currentQ[2] / currentDerivativeNorm;
+    let rotationMat = mat4.fromValues(cos, 0, sin, 0, 0, 1, 0, 0, -sin, 0, cos, 0, 0, 0, 0, 1);
+
+    mat4.identity(this.currentMatrix);
+    mat4.translate(this.currentMatrix, this.currentMatrix, vec3.fromValues(this.P1[0]+currentQ[0], this.P1[1]+currentQ[1], this.P1[2]+currentQ[2]));
+    mat4.multiply(this.currentMatrix, this.currentMatrix, rotationMat);
+}
+
+MyBezierAnimation.prototype.Q = function(s) {
+  let blend_1 = (1-s)*(1-s)*(1-s);
+	let blend_2 = 3*s*(1-s)*(1-s);
+	let blend_3 = 3*s*s*(1-s);
+	let blend_4 = s*s*s;
+
+	return vec3.fromValues(
+		blend_1*this.P1[0]+blend_2*this.P2[0]+blend_3*this.P3[0]+blend_4*this.P4[0],
+		blend_1*this.P1[1]+blend_2*this.P2[1]+blend_3*this.P3[1]+blend_4*this.P4[1],
+		blend_1*this.P1[2]+blend_2*this.P2[2]+blend_3*this.P3[2]+blend_4*this.P4[2]);
+}
+
+MyBezierAnimation.prototype.derivativeQ = function(s) {
+  let blend_1 = -3*(1-s)*(1-s);
+	let blend_2 = 3*(s-1)*(s-1.0/3.0);
+	let blend_3 = 3*s*(2-3*s);
+	let blend_4 = 3*s*s;
+
+	return vec3.fromValues(
+		blend_1*this.P1[0]+blend_2*this.P2[0]+blend_3*this.P3[0]+blend_4*this.P4[0],
+		blend_1*this.P1[1]+blend_2*this.P2[1]+blend_3*this.P3[1]+blend_4*this.P4[1],
+		blend_1*this.P1[2]+blend_2*this.P2[2]+blend_3*this.P3[2]+blend_4*this.P4[2]);
 }
