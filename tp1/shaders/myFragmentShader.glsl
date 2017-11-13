b7178627e585cabab4a5a109b813fdcac5bec216
cyrilico <jpdamas15@gmail.com>
2017-11-13 17:41:06 +0000

    Fix shaders (now consider object texture when it has one)

--- tp1/shaders/myFragmentShader.glsl
+++ tp1/shaders/myFragmentShader.glsl
@@ -3,9 +3,20 @@ precision highp float;
 #endif
 
 varying vec4 vFinalColor;
+varying vec2 vTextureCoord;
+
+uniform sampler2D uSampler;
 
 uniform float timeFactor;
 
+uniform bool uUseTexture;
+
 void main() {
-	gl_FragColor = mix(vFinalColor, vec4(1.0,0.0,0.0,1), timeFactor);
+	if (uUseTexture) {
+		vec4 textureColor = texture2D(uSampler, vTextureCoord);
+		gl_FragColor = textureColor * mix(vFinalColor, vec4(1.0,0.0,0.0,1), timeFactor);
+	}
+	else
+		gl_FragColor = mix(vFinalColor, vec4(1.0,0.0,0.0,1), timeFactor);
+
 }
