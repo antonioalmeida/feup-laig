6b97e56dbecd54b99f967c997416253475af0f08
theantonioalmeida <tony-alm@hotmail.com>
2017-12-12 01:19:27 +0000

    Closes #5 - subsitute MyQuad by MyPatch

--- tp3/game/MyBoard.js
+++ tp3/game/MyBoard.js
@@ -6,7 +6,7 @@ function MyBoard(game) {
     this.boardTexture = new CGFappearance(this.scene);
     this.boardTexture.loadTexture('textures/board2.png');
     this.defaultTexture = new CGFappearance(this.scene);
-    this.primitive = new MyQuad(this.scene, [-0.5, 0.5, 0.5, -0.5]); //TODO: Replace by MyPatch instance
+    this.primitive = new MyPatch(this.scene, [20,20], [[[ 0.5, 0.0, -0.5, 1], [0.5, 0.0, 0.5, 1] ], [[-0.5, 0.0, -0.5, 1], [-0.5, 0.0, 0.5, 1]]]);
     this.tiles = [];
     for(let i = 1; i <= 8; ++i)
         for(let j = 1; j <= 8; ++j)
@@ -26,7 +26,6 @@ MyBoard.prototype.display = function() {
     this.scene.clearPickRegistration();
 
     this.scene.scale(20, 1, 20);
-    this.scene.rotate(Math.PI/2, -1, 0, 0); //TODO: Remove after board is implemented via MyPatch
 
     this.boardTexture.apply();
     this.primitive.display();
--- tp3/game/MySidePlatform.js
+++ tp3/game/MySidePlatform.js
@@ -7,7 +7,7 @@ function MySidePlatform(game, sideSign) {
     this.platformTexture = new CGFappearance(this.scene);
     this.platformTexture.loadTexture('textures/platform.jpg');
     this.defaultTexture = new CGFappearance(this.scene);
-    this.primitive = new MyQuad(this.scene, [-0.5, 0.5, 0.5, -0.5]); //TODO: Replace by MyPatch instance
+    this.primitive = new MyPatch(this.scene, [20,20], [[[ 0.5, 0.0, -0.5, 1], [0.5, 0.0, 0.5, 1] ], [[-0.5, 0.0, -0.5, 1], [-0.5, 0.0, 0.5, 1]]]);
 }
 
 MySidePlatform.prototype = Object.create(CGFobject.prototype);
@@ -18,7 +18,6 @@ MySidePlatform.prototype.display = function() {
 
     this.scene.translate(15*this.sign, 0, 0);
     this.scene.scale(5, 1, 20);
-    this.scene.rotate(Math.PI/2, -1, 0, 0); //TODO: Remove after platform is implemented via MyPatch
 
     this.platformTexture.apply();
     this.primitive.display();
