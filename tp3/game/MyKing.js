aa38c05d6010b1a1666cc8e9d4d7adf7fee72499
theantonioalmeida <tony-alm@hotmail.com>
2017-12-21 17:05:46 +0000

    Add MyKing geometry with primitives

--- tp3/game/MyKing.js
+++ tp3/game/MyKing.js
@@ -1,7 +1,11 @@
 function MyKing(game, color, representation, initialPosition, file) {
     MyPiece.call(this, game, color, representation, initialPosition, file);
 
-    this.primitiveComponents = [new MyCylinder(game.scene, [2.5, 1, 0, 5, 10, 1, 1])];
+    this.primitiveComponents = [
+        new MyCylinder(game.scene,[1, 1, 1, 5, 10, 1, 1]),
+        new MyCylinder(game.scene,[1, 0.7, 0.3, 5, 10, 0, 0]),
+        new MyCube(game.scene)
+    ];
 }
 
 MyKing.prototype = Object.create(MyPiece.prototype);
@@ -10,8 +14,37 @@ MyKing.prototype.constructor = MyKing;
 MyKing.prototype.displayWithPrimitives = function() {
     this.scene.pushMatrix();
 
+    this.scene.pushMatrix();
     this.scene.rotate(-Math.PI/2, 1, 0, 0);
+    this.scene.scale(1, 1, 0.5);
     this.primitiveComponents[0].display();
+    this.scene.popMatrix();
+
+    this.scene.pushMatrix();
+    this.scene.rotate(-Math.PI/2, 1, 0, 0);
+    this.scene.translate(0, 0, 0.25);
+    this.scene.scale(1, 1, 1.5);
+    this.primitiveComponents[1].display();
+    this.scene.popMatrix();
+
+    this.scene.pushMatrix();
+    this.scene.rotate(-Math.PI/2, 1, 0, 0);
+    this.scene.translate(0, 0, 1.75);
+    this.scene.scale(0.3, 0.3, 1);
+    this.primitiveComponents[0].display();
+    this.scene.popMatrix();
+
+    this.scene.pushMatrix();
+   	this.scene.translate(0, 2.9, 0); 
+   	this.scene.scale(0.5, 0.3, 1.1);
+    this.primitiveComponents[2].display();
+    this.scene.popMatrix();
+
+    this.scene.pushMatrix();
+   	this.scene.translate(0, 3.15, 0); 
+   	this.scene.scale(0.5, 0.5, 0.2);
+    this.primitiveComponents[2].display();
+    this.scene.popMatrix();
 
     this.scene.popMatrix();
 }
